575b479f1d6745c56cbb76f69f5421a640e94f10
Quidsup <ooop.btinternet.com>
2016-03-01 22:36:32 +0000

    Pause feature added (Issue #19)

---
+++ admin/include/menu.js
@@ -0,0 +1,36 @@
+function PauseNoTrack(Action, PauseTime) {
+  switch (Action) {
+    case 'pause':
+      document.getElementById("dialogmsg").innerHTML = "Pausing NoTrack for "+PauseTime+" minutes";
+      break;
+    case 'start':
+      document.getElementById("dialogmsg").innerHTML = "Enabling NoTrack";
+      break;
+    case 'stop':
+      document.getElementById("dialogmsg").innerHTML = "Disabling NoTrack";
+      break;
+    default:
+      alert("PauseNoTrack: Unknown action");
+      return;
+  }
+  
+  document.getElementById("centerpoint1").style.display = "block";
+  document.getElementById("fade").style.display = "block";
+  
+  if (Action == "pause") {
+    window.open("?a="+Action+PauseTime, "_self");
+  }
+  else {  
+    window.open("?a="+Action, "_self");
+  }  
+}
+/*var urlParams = <?php echo json_encode($_GET, JSON_HEX_TAG);?>;*/
+//Options Box--------------------------------------------------------
+function ShowOptions() {
+  document.getElementById('centerpoint2').style.display = "block";
+  document.getElementById('fade').style.display = "block";
+}
+function HideOptions() {
+  document.getElementById('centerpoint2').style.display = "none";
+  document.getElementById('fade').style.display = "none";
+}
--- admin/include/topmenu.php
+++ admin/include/topmenu.php
@@ -1,4 +1,31 @@
 <?php
+if (isset($_GET['a'])) {
+  $Mem->delete('Config');
+  switch ($_GET['a']) {
+    case 'pause5':
+      ExecAction('pause5', true, true);
+      break;
+    case 'pause15':
+      ExecAction('pause15', true, true);
+      break;
+    case 'pause30':
+      ExecAction('pause30', true, true);
+      break;
+    case 'pause60':
+      ExecAction('pause60', true, true);
+      break;
+    case 'start':
+      ExecAction('start', true, true);
+      break;
+    case 'stop':
+      ExecAction('stop', true, true);
+      break;
+  }
+  sleep(5);  
+}
+
+LoadConfigFile();
+
 if ($_SERVER['PHP_SELF'] == '/admin/index.php') {
   echo '<div id="menu-top"><div id="menu-logo">'."\n";
   echo '<a href="../admin/"><img src="./svg/ntrklogo2.svg" alt=""></a></div>'."\n";
@@ -11,17 +38,55 @@ if ($_SERVER['PHP_SELF'] == '/admin/index.php') {
 }
 ?>
 <nav><div id="main-menu">
-  <a href="../admin"><span><img src="./svg/menu_home.svg" alt=""></span></a>
-  <a href="../admin/stats.php"><span><img src="./svg/menu_stats.svg" alt=""><span class="dtext">Domain Stats</span></span></a>
-  <a href="../admin/dhcpleases.php"><span><img src="./svg/menu_dhcp.svg" alt=""><span class="dtext">DHCP Leases</span></span></a>
-  <a href="../admin/config.php"><span><img src="./svg/menu_config.svg" alt=""><span class="dtext">Config</span></span></a>
-  <a href="../admin/system.php"><span><img src="./svg/menu_info.svg" alt=""><span class="dtext">System Info</span></span></a>
-  <a href="javascript:void(0)" onclick="document.getElementById('options-box').style.display='block';document.getElementById('fade').style.display='block'"><span class="float-right"><img src="./svg/menu_option.svg" alt=""><span class="dtext">Options</span></span></a>    
-</div></nav>
+  <a href="../admin"><span class="pictext"><img src="./svg/menu_home.svg" alt=""></span></a>
+  <a href="../admin/stats.php"><span class="pictext"><img src="./svg/menu_stats.svg" alt=""><span class="dtext">Domain Stats</span></span></a>
+  <a href="../admin/dhcpleases.php"><span class="pictext"><img src="./svg/menu_dhcp.svg" alt=""><span class="dtext">DHCP</span></span></a>
+  <a href="../admin/config.php"><span class="pictext"><img src="./svg/menu_config.svg" alt=""><span class="dtext">Config</span></span></a>
+  <a href="../admin/system.php"><span class="pictext"><img src="./svg/menu_info.svg" alt=""><span class="dtext">System Info</span></span></a>
+  <a href="#" onclick="ShowOptions()"><span class="pictext rightpictext"><img src="./svg/menu_option.svg" alt=""><span class="dtext">Options</span></span></a>
 
-<div id="options-box">
-<h2>Options</h2>
-<div id="close-button"><a href="javascript:void(0)" onclick="document.getElementById('options-box').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
+<?php
+//1. If Status = Paused & Enable Time < Now then switch Status to Enabled
 
-</div>
+if ((substr($Config['Status'], 0, 6) == 'Paused') && (floatval(substr($Config['Status'], 6))) < (time()+60)) {
+  $Mem->delete('Config');
+  LoadConfigFile();
+}
+
+echo '<div id="pause">'."\n";
+if (substr($Config['Status'], 0, 6) == 'Paused') {
+  echo '<span class="timer" title="Paused until">'.date('H:i', substr($Config['Status'], 6)).'</span>'."\n";
+  echo '<a href="#" onclick="PauseNoTrack(\'start\')"><span class="pbutton" title="Enable Blocking">&#9654;</span></a>'."\n";
+}
+elseif ($Config['Status'] == 'Stop') {
+  echo '<span class="timer" title="Disabled">----</span>'."\n";
+  echo '<a href="#" onclick="PauseNoTrack(\'start\')"><span class="pbutton" title="Enable Blocking">&#9654;</span></a>'."\n";
+}
+else {
+  echo '<a href="#" onclick="PauseNoTrack(\'stop\')"><span class="pbutton" title="Disable Blocking">&#8545;</span></a>'."\n";
+}
+echo '<div tabindex="1" id="dropbutton">&#x25BC;'."\n";
+echo '<div id="pause-menu">'."\n";
+echo '<a href="#" onclick="PauseNoTrack(\'pause\', 5)"><span>Pause for 5 minutes</span></a>'."\n";
+echo '<a href="#" onclick="PauseNoTrack(\'pause\', 15)"><span>Pause for 15 minutes</span></a>'."\n";
+echo '<a href="#" onclick="PauseNoTrack(\'pause\', 30)"><span>Pause for 30 minutes</span></a>'."\n";
+echo '<a href="#" onclick="PauseNoTrack(\'pause\', 60)"><span>Pause for 1 Hour</span></a>'."\n";
+echo "</div></div>\n";
+echo "</div></div></nav>\n";
+?>
+<div id="centerpoint1"><div id="dialog">
+<div class="dialog-bar">NoTrack</div>
+<span id="dialogmsg">Doing something</span>
+<div class="centered"><img src="./images/progress.gif"></div>
+</div></div>
+
+<div id="centerpoint2"><div id="options">
+<div class="dialog-bar">Options</div>
+<div id="close-button"><a href="#" onclick="HideOptions()"><img src="./svg/button_close.svg" onmouseover="this.src='./svg/button_close_over.svg'" onmouseout="this.src='./svg/button_close.svg'"></a></div>
+<br />
+<span><a href="#" class="button-grey">Update Trackers</a></span><br />
+<span><a href="#" class="button-grey">Restart</a></span>
+<span><a href="#" class="button-danger">Shutdown</a></span><br />
+</div></div>
+
 <div id="fade"></div>
