c4366315cbab1379644d5b20aff85186e32e1e0f
Quidsup <ooop.btinternet.com>
2016-03-18 22:28:48 +0000

    new help page

---
+++ admin/help.php
@@ -0,0 +1,56 @@
+<?php
+require('./include/global-vars.php');
+require('./include/global-functions.php');
+require('./include/topmenu.php');
+
+LoadConfigFile();
+if ($Config['Password'] != '') {  
+  session_start();  
+  if (! Check_SessionID()) {
+    header("Location: login.php");
+  }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <link href="./css/master.css" rel="stylesheet" type="text/css" />
+  <link rel="icon" type="image/png" href="./favicon.png" />
+  <script src="./include/menu.js"></script>
+  <title>NoTrack Help</title>
+</head>
+
+<body>
+<div id="main">
+<?php
+
+function LoadHelpPage($Page) {  
+  if (file_exists('./help/'.$Page.'.html')) {
+    echo file_get_contents('./help/'.$Page.'.html');
+  }
+  else {
+    echo 'Error: File not found'.PHP_EOL;
+  }
+}
+
+ActionTopMenu();
+DrawTopMenu();
+echo '<h1>NoTrack Help</h1>'.PHP_EOL;
+
+if (isset($_GET['p'])) {
+  switch($_GET['p']) {
+    case 'security':
+      LoadHelpPage('security');
+      break;
+    default:
+      LoadHelpPage('list');
+  }
+}
+else {
+  LoadHelpPage('list');
+}
+?>
+</div>
+</body>
+</html>
