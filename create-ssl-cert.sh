c5f071fb6f3fcad8c31327ef8ca41396eaa0b771
Quidsup <ooop.btinternet.com>
2016-06-28 18:01:29 +0000

    Issue #49 removed whiptail dependancy

--- create-ssl-cert.sh
+++ create-ssl-cert.sh
@@ -3,57 +3,36 @@
 #Description : This script will assist with creating and installing an SSL certificate on Lighttpd web server
 #Author : QuidsUp
 #Date 	: 2016-01-10
-#Version: v0.3
+#Version: v0.7.14
 #Usage 	: bash create-ssl-cert.sh
 
 #Program Settings----------------------------------------------------
-HostName=$(cat /etc/hostname)
-Height=$(tput lines)
-Width=$(tput cols)
-Height=$((Height / 2))
-Width=$(((Width * 2) / 3))
+HostName=$(hostname -f)
 
-#Welcome-------------------------------------------------------------
-Show_Welcome() {
-  whiptail --title "Welcome" --yesno "This installer will create an SSL Certificate on your NoTrack Webserver - Lighttpd" --yes-button "Ok" --no-button "Abort" 12 $Width
-  if (( $? == 1)) ; then                           #Abort install if user selected no
-    echo "Aborting Install"
-    exit 1
-  fi
-}
-
-#Install Complete----------------------------------------------------
-Show_Finish() {
-  whiptail --msgbox --title "Complete" "Your SSL Certificate has been installed and Lighttpd has sucessfully restarted" 10 $Width
-  echo
-  #echo "Install the $HostName-cert.p12 certificate into your web browser"
-  echo
-}
-
-#Root Warning (Incase user executes this script as root)-------------
-Show_RootWarning() {
-  whiptail --msgbox --title "Error" "Do not run this script as Root!\nExecute with: bash create-ssl-cert.sh" 10 $Width
-  exit 2
-}
 
 #Check if required applications are installed------------------------
 Check_AppsInstalled() {
-  command -v lighttpd >/dev/null 2>&1 || { echo "Lighttpd is not installed.  Aborting." >&2; exit 1; }
-  command -v openssl >/dev/null 2>&1 || { echo "OpenSSL is not installed.  Aborting." >&2; exit 1; }
+  if [[ $(command -v lighttpd) == "" ]]; then
+    echo "Lighttpd is not installed.  Aborting."
+    exit 41
+  fi
+  if [[ $(command -v openssl) == "" ]]; then
+    echo "OpenSSL is not installed.  Aborting."
+    exit 42
+  fi
 }
 
 #Main----------------------------------------------------------------
 if [ "$(id -u)" == "0" ]; then                   #Check if running as root
-   Show_RootWarning                              
+  echo "Error: Don't run this script as Root"
+  echo "Execute with: bash create-ssl-cert.sh"
+  exit 4  
 fi
-
-Show_Welcome
 
 Check_AppsInstalled                              #Check if required apps are installed
 
 clear
-echo "Enabling SSL Module on Lighttpd"
-sudo lighty-enable-mod ssl
+echo "This installer will create an SSL Certificate on your NoTrack Webserver - Lighttpd"
 echo
 
 echo "Example details required to create an SSL Certificate:"
@@ -67,13 +46,22 @@ echo "Email Address []: certs@quidsup.net"
 echo
 echo "Two letter Country Codes: https://www.digicert.com/ssl-certificate-country-codes.htm"
 echo
-read -n1 -r -p "Press any key to continue..."
+read -p "Continue (Y/n)? " -n1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Aborting"
+  exit 1
+fi
 
+#Start creating the certificate
+echo "Enabling SSL Module on Lighttpd"
+sudo lighty-enable-mod ssl
+echo
 
 openssl req -new -newkey rsa:2048 -nodes -sha256 -x509 -days 365 -keyout ~/server.key -out ~/server.crt
 if [ ! -e ~/server.key ] || [ ! -e ~/server.crt ]; then
   echo "Error creation of SSL certificate has failed.  Aborting"
-  exit 2
+  exit 43
 fi
 
 echo "Merging Crt file and Key file to form Pem"
@@ -95,15 +83,11 @@ sudo service lighttpd force-reload
 echo
 
 if [ -z "$(pgrep lighttpd)" ]; then                #Check if lighttpd restart has been successful
-  whiptail --title "Error" --yesno "Lighttpd restart has failed. Something is wrong in the Lighttpdconfig\n\nDo you want to revert back to old configuration?" --yes-button "Yes" --no-button "No" 14 $Width
-  if (( $? == 1)); then                            #Abort if user selected no
-    echo "Something may have gone wrong with the certificate settings."
-    echo "Try and re-run this script"
-    echo "If you want to try and manually fix the Lighttpd config you'll find it in /etc/lighttpd"
-    echo "Restart the service with: sudo service lighttpd force-reload"
-    exit 1
-  fi
+  echo "Lighttpd restart has failed."
+  echo "Something is wrong in the Lighttpdconfig"
+  echo
   
+  sleep 5s
   echo "Disabling Lighttpd SSL Module"
   sudo lighty-disable-mod ssl                      #Disable SSL Module
   echo "Restarting Lighttpd"
@@ -112,10 +96,12 @@ if [ -z "$(pgrep lighttpd)" ]; then                #Check if lighttpd restart ha
   
   if [ -z "$(pgrep lighttpd)" ]; then              #Check if lighttpd restart has now been successful
     echo "Lighttpd restart failed"
+    exit 44
   else
     echo "Lighttpd restart successful"
+    exit 45
   fi
-else
-  Show_Finish
 fi
 
+echo "SSL Certificate has been installed and Lighttpd has sucessfully restarted"
+#echo "Install the $HostName-cert.p12 certificate into your web browser"
