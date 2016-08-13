1481a22335c6ecca41b94f642de6de548322082a
Quidsup <ooop.btinternet.com>
2016-08-13 11:56:21 +0000

    changelog displayed by default

--- admin/upgrade.php
+++ admin/upgrade.php
@@ -65,21 +65,21 @@ else {                                           //Just displaying status
     DrawSysRow('Commence Upgrade', '<button class="button-blue" onclick="window.location=\'?u=1\'">Upgrade</button>');
     echo '</table>'.PHP_EOL;
     echo '</div></div>'.PHP_EOL;
-
-    //Display changelog
-    if (extension_loaded('curl')) {              //Check if user has Curl installed
-      $ch = curl_init();                         //Initiate curl
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
-      curl_setopt($ch, CURLOPT_URL,'https://raw.githubusercontent.com/quidsup/notrack/master/changelog.txt');
-      $Data = curl_exec($ch);                    //Download Changelog
-      curl_close($ch);                           //Close curl
-      echo '<pre>'.PHP_EOL;
-      echo $Data;
-      echo '</pre>'.PHP_EOL;;
-    }
-  }
+   }
+   
+   //Display changelog
+   if (extension_loaded('curl')) {               //Check if user has Curl installed
+    $ch = curl_init();                           //Initiate curl
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
+    curl_setopt($ch, CURLOPT_URL,'https://raw.githubusercontent.com/quidsup/notrack/master/changelog.txt');
+    $Data = curl_exec($ch);                      //Download Changelog
+    curl_close($ch);                             //Close curl
+    echo '<pre>'.PHP_EOL;
+    echo $Data;                                  //Display Data
+    echo '</pre>'.PHP_EOL;;
+  }  
 }
 ?> 
 </div>
