954cca3d45f7af24ba98a335a4b2f4ff685639b1
fernfrost <martin.h.nilsson@gmail.com>
2016-08-15 23:14:11 +0200

    Cleans up formatting

--- ask_os.sh
+++ ask_os.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
 Ask_Os(){
-    local Os_Id=""
-    local Os_Version=""
-    local Os_Description=""
+  local Os_Id=""
+  local Os_Version=""
+  local Os_Description=""
     
-    if [ -e /etc/lsb-release ]; then
-        Os_Id=$(cat /etc/lsb-release | grep "DISTRIB_ID" | cut -d "=" -f 2)
-        Os_Version=$(cat /etc/lsb-release | grep "DISTRIB_RELEASE" | cut -d "=" -f 2)
-        Os_Description=$(cat /etc/lsb-release | grep "DISTRIB_DESCRIPTION" | cut -d "=" -f 2 | cut -d "\"" -f 2)
-    elif [ -e /etc/os-release ]; then
-        #Raspbian Jessie
-        Os_Id=$(cat /etc/os-release | grep "^ID=" | cut -d "=" -f 2)
-        Os_Version=$(cat /etc/os-release | grep "VERSION_ID" | cut -d "=" -f 2 | cut -d "\"" -f 2)
-        Os_Description=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d "=" -f 2 | cut -d "\"" -f 2)
-    else
-        echo "Warning: Unable to determine OS"
-    fi
+  if [ -e /etc/lsb-release ]; then
+    Os_Id=$(cat /etc/lsb-release | grep "DISTRIB_ID" | cut -d "=" -f 2)
+    Os_Version=$(cat /etc/lsb-release | grep "DISTRIB_RELEASE" | cut -d "=" -f 2)
+    Os_Description=$(cat /etc/lsb-release | grep "DISTRIB_DESCRIPTION" | cut -d "=" -f 2 | cut -d "\"" -f 2)
+  elif [ -e /etc/os-release ]; then
+  #Raspbian Jessie
+    Os_Id=$(cat /etc/os-release | grep "^ID=" | cut -d "=" -f 2)
+    Os_Version=$(cat /etc/os-release | grep "VERSION_ID" | cut -d "=" -f 2 | cut -d "\"" -f 2)
+    Os_Description=$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d "=" -f 2 | cut -d "\"" -f 2)
+  else
+    echo "Warning: Unable to determine OS"
+  fi
 
-
-        echo $Os_Id
-        echo $Os_Version
-        echo $Os_Description
+  echo $Os_Id
+  echo $Os_Version
+  echo $Os_Description
 }
 
 
