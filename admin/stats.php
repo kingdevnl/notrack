1c5d5556ca5e81b2d97cde0fcb2599f1b21e0bc5
Quidsup <ooop.btinternet.com>
2016-08-21 10:31:05 +0000

    fixed Issue #77 for Aussie sites

--- admin/stats.php
+++ admin/stats.php
@@ -56,7 +56,7 @@ function ReturnURL($Str) {
   if (substr($Str,0,4) == 'www.') $Site = substr($Str,4); 
   else $Site = $Str;
   
-  if (preg_match('/[A-Za-z1-9\-]{2,63}\.(org\.|co\.|au\.)?[A-Za-z1-9\-]{2,63}$/', $Site, $Match) == 1) {
+  if (preg_match('/[A-Za-z1-9\-]{2,63}\.(org\.|co\.|com\.)?[A-Za-z1-9\-]{2,63}$/', $Site, $Match) == 1) {
     if (in_array($Match[0],$CommonSites)) return '*.'.$Match[0];
     else return $Site;
   }
