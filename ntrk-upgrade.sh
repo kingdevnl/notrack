1043e50f4760f3b0ac757aa21fada6af76a62041
Quidsup <ooop.btinternet.com>
2016-10-16 15:57:36 +0100

    added ntrk parser

--- ntrk-upgrade.sh
+++ ntrk-upgrade.sh
@@ -16,11 +16,11 @@
 ConfigFile="/etc/notrack/notrack.conf"
 
 #Variables-----------------------------------------------------------
-InstallLoc=""
+INSTALL_LOCATION=""
 UserName=""
 
 #--------------------------------------------------------------------
-Check_File_Exists() {
+check_file_exists() {
   if [ ! -e "$1" ]; then
     echo "Error file $1 is missing.  Aborting."
     exit 24
@@ -34,23 +34,23 @@ fi
 
 for HomeDir in /home/*; do
   if [ -d "$HomeDir/NoTrack" ]; then 
-    InstallLoc="$HomeDir/NoTrack"
+    INSTALL_LOCATION="$HomeDir/NoTrack"
     break
   elif [ -d "$HomeDir/notrack" ]; then 
-    InstallLoc="$HomeDir/notrack"
+    INSTALL_LOCATION="$HomeDir/notrack"
     break
   fi
 done
 
-if [[ $InstallLoc == "" ]]; then
+if [[ $INSTALL_LOCATION == "" ]]; then
   if [ -d "/opt/notrack" ]; then
-    InstallLoc="/opt/notrack"
+    INSTALL_LOCATION="/opt/notrack"
     UserName="root"
   elif [ -d "/root/notrack" ]; then
-    InstallLoc="/root/notrack"
+    INSTALL_LOCATION="/root/notrack"
     UserName="root"
   elif [ -d "/notrack" ]; then
-    InstallLoc="/notrack"
+    INSTALL_LOCATION="/notrack"
     UserName="root"
   else
     echo "Error Unable to find NoTrack folder"
@@ -61,29 +61,29 @@ else
   UserName=$(grep "$HomeDir" /etc/passwd | cut -d : -f1)
 fi
 
-echo "Install Location $InstallLoc"
+echo "Install Location $INSTALL_LOCATION"
 echo "Username: $UserName"
 
 #Alt command for sudoless systems
 #su -c "cd /home/$USERNAME/$PROJECT ; svn update" -m "$USERNAME" 
 
 sudo -u $UserName bash << ROOTLESS
-if [ "$(command -v git)" ]; then               #Utilise Git if its installed
+if [ "$(command -v git)" ]; then                 #Utilise Git if its installed
   echo "Pulling latest updates of NoTrack using Git"
-  cd "$InstallLoc"
+  cd "$INSTALL_LOCATION"
   git pull
-  if [ $? != "0" ]; then                       #Git repository not found
+  if [ $? != "0" ]; then                         #Git repository not found
     echo "Git repository not found"
-    if [ -d "$InstallLoc-old" ]; then          #Delete NoTrack-old folder if it exists
+    if [ -d "$INSTALL_LOCATION-old" ]; then      #Delete NoTrack-old folder if it exists
       echo "Removing old NoTrack folder"
-      rm -rf "$InstallLoc-old"
+      rm -rf "$INSTALL_LOCATION-old"
     fi
-    echo "Moving $InstallLoc folder to $InstallLoc-old"
-    mv "$InstallLoc" "$InstallLoc-old"
-    echo "Cloning NoTrack to $InstallLoc with Git"
-    git clone --depth=1 https://github.com/quidsup/notrack.git "$InstallLoc"
+    echo "Moving $INSTALL_LOCATION folder to $INSTALL_LOCATION-old"
+    mv "$INSTALL_LOCATION" "$INSTALL_LOCATION-old"
+    echo "Cloning NoTrack to $INSTALL_LOCATION with Git"
+    git clone --depth=1 https://github.com/quidsup/notrack.git "$INSTALL_LOCATION"
   fi
-else                                           #Git not installed, fallback to wget
+else                                             #Git not installed, fallback to wget
   echo "Downloading latest version of NoTrack from https://github.com/quidsup/notrack/archive/master.zip"
   wget -O /tmp/notrack-master.zip https://github.com/quidsup/notrack/archive/master.zip
   if [ ! -e /tmp/notrack-master.zip ]; then    #Check to see if download was successful
@@ -92,19 +92,19 @@ else                                           #Git not installed, fallback to w
     exit 23
   fi
   
-  if [ -d "$InstallLoc" ]; then                  #Check if NoTrack folder exists  
-    if [ -d "$InstallLoc-old" ]; then            #Delete NoTrack-old folder if it exists
+  if [ -d "$INSTALL_LOCATION" ]; then            #Check if NoTrack folder exists  
+    if [ -d "$INSTALL_LOCATION-old" ]; then      #Delete NoTrack-old folder if it exists
       echo "Removing old NoTrack folder"
-      rm -rf "$InstallLoc-old"
+      rm -rf "$INSTALL_LOCATION-old"
     fi
-    echo "Moving $InstallLoc folder to $InstallLoc-old"
-    mv "$InstallLoc" "$InstallLoc-old"
+    echo "Moving $INSTALL_LOCATION folder to $INSTALL_LOCATION-old"
+    mv "$INSTALL_LOCATION" "$INSTALL_LOCATION-old"
   fi
  
   echo "Unzipping notrack-master.zip"
   unzip -oq /tmp/notrack-master.zip -d /tmp
-  echo "Copying folder across to $InstallLoc"
-  mv /tmp/notrack-master "$InstallLoc"
+  echo "Copying folder across to $INSTALL_LOCATION"
+  mv /tmp/notrack-master "$INSTALL_LOCATION"
   echo "Removing temporary files"
   rm /tmp/notrack-master.zip                     #Cleanup
 fi
@@ -115,33 +115,33 @@ if [ $? == 23 ]; then                            #Code hasn't downloaded
   exit 23
 fi
 
-Check_File_Exists "$InstallLoc/notrack.sh"       #NoTrack.sh
+check_file_exists "$INSTALL_LOCATION/notrack.sh"       #NoTrack.sh
 echo "Updating notrack.sh"
-cp "$InstallLoc/notrack.sh" /usr/local/sbin/
+cp "$INSTALL_LOCATION/notrack.sh" /usr/local/sbin/
 mv /usr/local/sbin/notrack.sh /usr/local/sbin/notrack
 chmod 755 /usr/local/sbin/notrack
   
-Check_File_Exists "$InstallLoc/ntrk-exec.sh"     #ntrk-exec.sh
+check_file_exists "$INSTALL_LOCATION/ntrk-exec.sh"     #ntrk-exec.sh
 echo "Updating ntrk-exec.sh"
-cp "$InstallLoc/ntrk-exec.sh" /usr/local/sbin/
+cp "$INSTALL_LOCATION/ntrk-exec.sh" /usr/local/sbin/
 mv /usr/local/sbin/ntrk-exec.sh /usr/local/sbin/ntrk-exec
 chmod 755 /usr/local/sbin/ntrk-exec
   
-Check_File_Exists "$InstallLoc/ntrk-pause.sh"    #ntrk-pause.sh
+check_file_exists "$INSTALL_LOCATION/ntrk-pause.sh"    #ntrk-pause.sh
 echo "Updating ntrk-pause.sh"
-cp "$InstallLoc/ntrk-pause.sh" /usr/local/sbin/
+cp "$INSTALL_LOCATION/ntrk-pause.sh" /usr/local/sbin/
 mv /usr/local/sbin/ntrk-pause.sh /usr/local/sbin/ntrk-pause
 chmod 755 /usr/local/sbin/ntrk-pause
 
-Check_File_Exists "$InstallLoc/ntrk-upgrade.sh"  #ntrk-upgrade.sh
+check_file_exists "$INSTALL_LOCATION/ntrk-upgrade.sh"  #ntrk-upgrade.sh
 echo "Updating ntrk-upgrade.sh"
-cp "$InstallLoc/ntrk-upgrade.sh" /usr/local/sbin/
+cp "$INSTALL_LOCATION/ntrk-upgrade.sh" /usr/local/sbin/
 mv /usr/local/sbin/ntrk-upgrade.sh /usr/local/sbin/ntrk-upgrade
 chmod 755 /usr/local/sbin/ntrk-upgrade
 
-Check_File_Exists "$InstallLoc/scripts/ntrk-parse.sh"      #ntrk-parse.sh
+check_file_exists "$INSTALL_LOCATION/scripts/ntrk-parse.sh"      #ntrk-parse.sh
 echo "Updating ntrk-parse.sh"
-cp "$InstallLoc/scripts/ntrk-parse.sh" /usr/local/sbin/
+cp "$INSTALL_LOCATION/scripts/ntrk-parse.sh" /usr/local/sbin/
 mv /usr/local/sbin/ntrk-parse.sh /usr/local/sbin/ntrk-parse
 chmod 755 /usr/local/sbin/ntrk-parse
   
